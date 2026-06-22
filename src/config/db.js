ad6b6a110213ec5e124f4d12e8fb0cc20edf8c64
Nick <HypeBass.LifeBalance@gmail.com>
2026-06-22 01:25:55 -0300

    idk, im going crazy. Configurado os arquivos server.js e db.js para a comunicacao com o banco de dados.

---
+++ src/config/db.js
@@ -0,0 +1,15 @@
+import "dotenv/config";
+
+import { PrismaClient } from "../../generated/prisma/index.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+const adapter = new PrismaPg({
+	connectionString: process.env.DATABASE_URL,
+});
+const prisma = new PrismaClient({ adapter });
+
+async function getUser() {
+	const user = await prisma.user.findMany();
+	console.log(user);
+}
+
+getUser();
