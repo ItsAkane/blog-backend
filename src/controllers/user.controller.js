59ee943b725481e4ba221b02bf7f038aca094922
Nick <HypeBass.LifeBalance@gmail.com>
2026-06-28 19:43:29 -0300

    feat: adicionados metodos delete e update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -18,4 +18,22 @@ async function getAll(req, res) {
 	}
 }
 
-export default { create, getAll };
+async function update(req, res) {
+	try {
+		const user = await userService.updateUser(req.params.id, req.body);
+		res.status(200).json(user);
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+}
+
+async function remove(req, res) {
+	try {
+		await userService.deleteUser(req.params.id);
+		res.status(204).send();
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
+}
+
+export default { create, getAll, update, remove };
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -8,7 +8,20 @@ async function getAllUsers() {
 	return prisma.user.findMany();
 }
 
+async function updateUser(id, data) {
+	return prisma.user.update({
+		where: { id: Number(id) },
+		data,
+	});
+}
+
+async function deleteUser(id) {
+	return prisma.user.delete({ where: { id: Number(id) } });
+}
+
 export default {
 	createUser,
 	getAllUsers,
+	updateUser,
+	deleteUser,
 };
